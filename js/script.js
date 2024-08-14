9c9a42b1b20ef8fab259be7798edb08059f2af88
FadliArifRamadan <ramadanfadliarif@gmail.com>
2024-08-15 01:25:30 +0700

    membuat UI search

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// Toggle class active
-const navbarNav = document.querySelector('.navbar-nav');
+// Toggle class active untk menu
+const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika menu diklik
-document.querySelector('#menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-// klik diluar sidebar untuk menghilangan nav
-const menu = document.querySelector('#menu');
+// Toogle class active untuk seacrh form
+const seacrhForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function (e) {
-    if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+// ketika search diklik
+document.querySelector("#search").onclick = (e) => {
+  seacrhForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// klik diluar elemen
+const menu = document.querySelector("#menu");
+const seacrh = document.querySelector("#search");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!search.contains(e.target) && !seacrhForm.contains(e.target)) {
+    seacrhForm.classList.remove("active");
+  }
 });
